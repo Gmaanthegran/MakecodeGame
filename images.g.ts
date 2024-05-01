b875eeb2220d6bf617b8e13501eece91204a41a7
Gmaanthegran <144182436+Gmaanthegran@users.noreply.github.com>
2024-04-30 20:12:34 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,58 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "Demon":return img`
+2 . . . 2 
+2 2 2 2 2 
+2 5 2 5 2 
+2 2 2 2 2 
+. . 2 . . 
+. 2 2 2 . 
+2 . 2 . 2 
+. 2 . 2 . 
+`;
+            case "image1":
+            case "playerframe1":return img`
+. 2 2 2 . 
+. e e e 2 
+e e d f d 
+. d d d . 
+. 2 2 2 . 
+. 2 5 2 2 
+. 2 2 2 . 
+. 8 . 8 . 
+`;
+            case "image3":
+            case "playerframe2":return img`
+. 2 2 2 . 
+2 e e e . 
+e e d f d 
+. d d d . 
+. 2 2 2 2 
+. 2 5 2 . 
+. 2 2 2 . 
+8 . . 8 . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
